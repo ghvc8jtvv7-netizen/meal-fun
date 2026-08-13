f55f89b414a98442ad69840f44555a11f31eb5c9
Kent Woon <ghvc8jtvv7@privaterelay.appleid.com>
2026-08-13 15:52:59 +0800

    bug fixes

--- gentle-fuel-pages/sw.js
+++ gentle-fuel-pages/sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'gentle-fuel-v12';
+const CACHE = 'gentle-fuel-v14';
 const FILES = [
   './index.html',
   './manifest.webmanifest',
--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'gentle-fuel-v12';
+const CACHE = 'gentle-fuel-v14';
 const FILES = [
   './gentle-fuel.html',
   './manifest.webmanifest',
